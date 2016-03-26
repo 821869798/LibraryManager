96548fe6d18a6a0db4c85e4974b6fa72cbe7eec1
bu fan <bu fan>
2016-03-26 20:02:37 +0800

    ...

--- LibraryServer/models.py
+++ LibraryServer/models.py
@@ -4,4 +4,12 @@ class RootAdminer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20),unique=True,nullable=False)
     password = db.Column(db.String(20),nullable=False)
-    
+
+
+class Adminer(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20),unique=True,nullable=False)
+    password = db.Column(db.String(20),nullable=False)
+    bookmanage = db.Column(db.Integer,nullable=False,default=0)
+    readermanage = db.Column(db.Integer,nullable=False,default=0)
+    name = db.Column(db.String(64))
--- LibraryServer/views.py
+++ LibraryServer/views.py
@@ -1,5 +1,8 @@
 from models import db
 from config import app
+from flask import session
+from flask import request
+from flask import make_response
 
 @app.route("/login",methods=["POST","GET"])
 def login():
